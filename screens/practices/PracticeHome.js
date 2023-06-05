bcf983549145e02904b5bbd4ced6a5bcd951c66a
JJoeYi <joelowyi@gmail.com>
2023-06-05 22:34:45 +0800

    add bottom tabs and GlobalColors for theming

---
+++ screens/practices/PracticeHome.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function PracticeHome({ navigation, route }) {
+  return (
+    <View>
+      <Text> Practice home page!</Text>
+    </View>
+  );
+}
---
+++ screens/rewards/RewardsScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function RewardsScreen({ navigation, route }) {
+  return (
+    <View>
+      <Text> Rewards Screen! </Text>
+    </View>
+  );
+}
