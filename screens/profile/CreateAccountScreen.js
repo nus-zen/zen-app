e631998816560b6aa23a277e13d5535a8c33aa2a
JJoeYi <joelowyi@gmail.com>
2023-06-10 14:54:55 +0800

    remove yarn and add package-lock.json

---
+++ screens/profile/CreateAccountScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CreateAccountScreen = () => {
+  return (
+    <View>
+      <Text>Welcome to the Create Account Screen!</Text>
+    </View>
+  );
+};
+
+export default CreateAccountScreen;
