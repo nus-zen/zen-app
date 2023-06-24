f6ad7c57b52171365c7f4a44a8db2c247bcfbe8c
lukellim <lukellim@u.nus.edu>
2023-06-25 00:48:51 +0800

    Rewards Page (Categories)

---
+++ data/RewardsData.js
@@ -0,0 +1,41 @@
+export const REWARDS_DATA = [
+    {
+      title: "F&B",
+      subtitle: "On Campus",
+      imageSource: "https://uci.nus.edu.sg/oca/wp-content/uploads/sites/9/2018/05/Fine-Food-1-1024x684.jpg",
+      description:
+        "Redeem your favourite F&B items conveniently on campus by presenting a generated QR code to the store vendor after selecting the item of your choice. Enjoy it with a friend!",
+      rationale:
+        "Enjoy some refreshments amidst your busy schedule. Food is a source of fuel to power up your system so you can focus on your tasks better.",
+    },
+    {
+      title: "F&B",
+      subtitle: "Off Campus",
+      imageSource:
+        "https://static.wixstatic.com/media/4aa710_c3fb6fa3be6e492bbfc293414b8b0890~mv2.jpg/v1/fill/w_640,h_452,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/4aa710_c3fb6fa3be6e492bbfc293414b8b0890~mv2.jpg",
+      description:
+        "Redeem F&B items conveniently on the go by presenting a generated QR code to the store vendor after selecting the item of your choice. Enjoy it with a friend!",
+      rationale:
+        "Enjoy some refreshments amidst your busy schedule. Food is a source of fuel to power up your system so you can focus on your tasks better.",
+    },
+    {
+      title: "Meditation",
+      subtitle: "Enhancement",
+      imageSource:
+        "https://i.etsystatic.com/21554180/r/il/7ea8e1/3702142142/il_fullxfull.3702142142_6y88.jpg",
+      description:
+        "Explore various methods of enhancement to improve the quality of your meditation.",
+      rationale:
+        "Widely-used meditation techniques from different regions to stimulate your body and mind. Try out what interests you! ",
+    },
+    {
+      title: "Terrarium",
+      subtitle: "Supplies",
+      imageSource:
+        "https://simplysucculents.com/wp-content/uploads/2021/09/DSC_2189-600x600.jpg",
+      description:
+        "Redeem additional supplies to add to your terrarium. Seasonal items offered for a limited period of time.",
+      rationale:
+        "The only constant in life is change. Do not be afraid of changing your surroundings once in a while.",
+    },
+  ];
