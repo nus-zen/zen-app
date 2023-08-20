158a6b9028a88779703186b5330749f2ea805171
JJoeYi <joelowyi@gmail.com>
2023-08-20 21:00:28 +0800

    z

--- screens/practices/PracticeMediaScreen.js
+++ screens/practices/PracticeMediaScreen.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
-import YouTubePlayer from "../../components/YouTubePlayer";
+// import YouTubePlayer from "../../components/YouTubePlayer";
 
 export default function PracticeMediaScreen({ route }) {
   const { videoId, title } = route.params;
   return (
     <View style={styles.container}>
-      <YouTubePlayer videoId={videoId} title={title} />
+      {/* <YouTubePlayer videoId={videoId} title={title} /> */}
+      <Text>Temporary NO YOUTUBE PLAYER </Text>
     </View>
   );
 }
