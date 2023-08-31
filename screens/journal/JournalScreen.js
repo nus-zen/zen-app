03e415f54b71d191fb2d29dac28c9e342ec551f2
raven0205 <73115813+raven0205@users.noreply.github.com>
2023-09-01 04:17:31 +0800

    Update JournalScreen.js
    update to sticky note journaling with date time, edit delete buttons feature

--- screens/journal/JournalScreen.js
+++ screens/journal/JournalScreen.js
@@ -1,32 +1,101 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, ScrollView, StyleSheet } from "react-native";
 import { GlobalColors } from "../../themes/GlobalColors";
 
 export default function JournalScreen() {
-  const [entry, setEntry] = useState("");
+  const [entryTitle, setEntryTitle] = useState("");
+  const [entryText, setEntryText] = useState("");
+  const [entries, setEntries] = useState([]);
+  const [editingEntry, setEditingEntry] = useState(null);
 
   const handleSaveEntry = () => {
-    // Save the journal entry
-    console.log("Saved entry:", entry);
-    // Reset the input field
-    setEntry("");
+    if (entryTitle.trim() !== "" && entryText.trim() !== "") {
+      if (editingEntry !== null) {
+        // Update the edited entry
+        const updatedEntries = entries.map((item) => {
+          if (item.id === editingEntry) {
+            return { ...item, title: entryTitle, text: entryText };
+          }
+          return item;
+        });
+        setEntries(updatedEntries);
+        setEditingEntry(null);
+      } else {
+        // Save a new entry
+        const newEntry = {
+          id: new Date().getTime(),
+          title: entryTitle,
+          text: entryText,
+          date: new Date().toLocaleString(),
+        };
+        setEntries([...entries, newEntry]);
+      }
+
+      // Clear input fields
+      setEntryTitle("");
+      setEntryText("");
+    }
+  };
+
+  const handleEditEntry = (id) => {
+    const entryToEdit = entries.find((item) => item.id === id);
+    if (entryToEdit) {
+      setEntryTitle(entryToEdit.title);
+      setEntryText(entryToEdit.text);
+      setEditingEntry(id);
+    }
+  };
+
+  const handleDeleteEntry = (id) => {
+    const updatedEntries = entries.filter((item) => item.id !== id);
+    setEntries(updatedEntries);
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>DRAFT JOURNAL SCREEN</Text>
+      <Text style={styles.title}>Sticky Notes Journal</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Title..."
+        value={entryTitle}
+        onChangeText={setEntryTitle}
+      />
       <TextInput
         style={styles.input}
         multiline
-        placeholder="Write your journal entry... (logs to console)  "
-        value={entry}
-        onChangeText={setEntry}
+        placeholder="Start typing here..."
+        value={entryText}
+        onChangeText={setEntryText}
       />
-      <Button
-        title="Save Entry"
+      <TouchableOpacity
+        style={styles.addButton}
         onPress={handleSaveEntry}
-        color={GlobalColors.primary300}
-      />
+      >
+        <Text style={styles.buttonText}>{editingEntry !== null ? "Update Entry" : "Save Entry"}</Text>
+      </TouchableOpacity>
+      <ScrollView style={styles.entriesContainer}>
+        {entries.map((item) => (
+          <View key={item.id} style={styles.entry}>
+            <Text style={styles.entryTitle}>{item.title}</Text>
+            <Text style={styles.entryText}>{item.text}</Text>
+            <Text style={styles.entryDate}>{item.date}</Text>
+            <View style={styles.entryActions}>
+              <TouchableOpacity
+                onPress={() => handleEditEntry(item.id)}
+                style={styles.editButton}
+              >
+                <Text style={styles.editButtonText}>Edit</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => handleDeleteEntry(item.id)}
+                style={styles.deleteButton}
+              >
+                <Text style={styles.deleteButtonText}>Delete</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -34,19 +103,82 @@ export default function JournalScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+    padding: 20,
+    backgroundColor: "#f9f9f9",
   },
   title: {
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: "bold",
-    marginBottom: 16,
+    marginBottom: 20,
+    color: GlobalColors.primary500,
   },
   input: {
-    height: 200,
-    borderColor: "gray",
+    height: 40,
+    borderColor: "#ccc",
     borderWidth: 1,
     borderRadius: 8,
-    marginBottom: 16,
+    marginBottom: 10,
     padding: 8,
+    fontSize: 16,
+    backgroundColor: "#fff",
+    color: "#333",
+  },
+  addButton: {
+    backgroundColor: GlobalColors.primary300,
+    paddingVertical: 12,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  entriesContainer: {
+    flex: 1,
+  },
+  entry: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    marginBottom: 10,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: "#ddd",
+  },
+  entryTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 5,
+    color: GlobalColors.primary500,
+  },
+  entryText: {
+    fontSize: 16,
+    color: "#333",
+  },
+  entryDate: {
+    fontSize: 12,
+    color: "#888",
+    marginTop: 5,
+  },
+  entryActions: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginTop: 10,
+  },
+  editButton: {
+    marginRight: 10,
+    paddingVertical: 5,
+  },
+  editButtonText: {
+    color: GlobalColors.primary300,
+    fontSize: 16,
+  },
+  deleteButton: {
+    paddingVertical: 5,
+  },
+  deleteButtonText: {
+    color: "red",
+    fontSize: 16,
   },
 });
