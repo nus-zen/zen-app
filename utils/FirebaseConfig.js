066fabd5fa142f42f692fabb6eb88f1095714268
shuhuiii <97970820+shuhuiii@users.noreply.github.com>
2023-10-09 23:51:48 +0800

    fix bugs in firebase

--- utils/FirebaseConfig.js
+++ utils/FirebaseConfig.js
@@ -1,9 +1,5 @@
-import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  getReactNativePersistence,
-  initializeAuth,
-} from "firebase/auth";
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth, getReactNativePersistence, initializeAuth, } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
