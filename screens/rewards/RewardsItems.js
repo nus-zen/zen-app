427db11fec90d53fb53667f0bdc105731b0ceb3c
shuhuiii <97970820+shuhuiii@users.noreply.github.com>
2023-10-22 22:41:34 +0800

    fix bugs in rewards

--- screens/rewards/RewardsItems.js
+++ screens/rewards/RewardsItems.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, StyleSheet, FlatList, Image, Dimensions, } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, FlatList, Image, Dimensions } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const RewardsItems = ({ route }) => {
-  const { selectedCard } = route.params || {}; 
+export default function RewardsItems({ route }) {
+// export default function RewardsItems ({ route }) => {
+  const { selectedCard } = route.params || {};
   const [STpoints, setSTPoints] = useState(0);
   const [vouchers, setVouchers] = useState([]);
-  
+
   useEffect(() => {
     console.log(`Selected card index: ${selectedCard}`);
     if (selectedCard !== undefined) {
@@ -14,64 +16,60 @@ const RewardsItems = ({ route }) => {
     }
   }, [selectedCard]);
 
-    const updateVouchers = (selectedCard) => {
-
-      switch (selectedCard) {
-        case 0:
-
+  const updateVouchers = (selectedCard) => {
+    switch (selectedCard) {
+      case 0:
         setVouchers([
-            { id: 1, name: "$2 KOI Voucher", coins: 50, count: 0 },
-            { id: 2, name: "$3 Awfully Chocolate Voucher", coins: 70, count: 0 },
-            { id: 3, name: "$3 TWG Tea Voucher", coins: 70, count: 0 },
-            { id: 4, name: "$4 Crave Voucher", coins: 60, count: 0 },
-            { id: 5, name: "$4 Cat & The Fiddle Voucher", coins: 80, count: 0 },
-            { id: 6, name: "$6 Birds of Paradise Voucher", coins: 90, count: 0 },
-          ]);
-          break;
-
-        case 1:
-          setVouchers([
-            { id: 7, name: "$1 Drinks Voucher", coins: 70, count: 0 },
-            { id: 8, name: "$1 Food Voucher", coins: 40, count: 0 },
-            { id: 9, name: "$2 Starbucks Voucher", coins: 70, count: 0 },
-            { id: 10, name: "$2 LiHo Voucher", coins: 70, count: 0 },
-            { id: 11, name: "$2 Spinelli Voucher", coins: 70, count: 0 },
-          ]);
-          break;
-        case 2:
-          setVouchers([
-            { id: 12, name: "Forest Green Yarn", coins: 80, count: 0 },
-            { id: 13, name: "Olive Green Yarn", coins: 60, count: 0 },
-            { id: 14, name: "Grass Green Yarn", coins: 80, count: 0 },
-            { id: 15, name: "Yellow Green Yarn", coins: 90, count: 0 },
-            { id: 16, name: "Crochet Hook", coins: 80, count: 0 },
-            { id: 17, name: "Tape", coins: 30, count: 0 },
-          ]);
-          break;
-        case 3:
-          setVouchers([
-            { id: 18, name: "Soil (50g)", coins: 30, count: 0 },
-            { id: 19, name: "Air Plant", coins: 40, count: 0 },
-            { id: 20, name: "Pebbles (50g)", coins: 50, count: 0 },
-            { id: 21, name: "Blue Sand (50g)", coins: 50, count: 0 },
-            { id: 22, name: "Plastic Enclosure", coins: 70, count: 0 },
-            { id: 23, name: "Mini House", coins: 70, count: 0 },
-            { id: 24, name: "Mini Duck", coins: 70, count: 0 },
-            { id: 25, name: "Mini Rabbit", coins: 70, count: 0 },
-          ]);
-          break;
-        default:
-          setVouchers([]);
-          break;
-      }
-    };
+          { id: 1, name: "$2 KOI Voucher", coins: 50, count: 0 },
+          { id: 2, name: "$3 Awfully Chocolate Voucher", coins: 70, count: 0 },
+          { id: 3, name: "$3 TWG Tea Voucher", coins: 70, count: 0 },
+          { id: 4, name: "$4 Crave Voucher", coins: 60, count: 0 },
+          { id: 5, name: "$4 Cat & The Fiddle Voucher", coins: 80, count: 0 },
+          { id: 6, name: "$6 Birds of Paradise Voucher", coins: 90, count: 0 },
+        ]);
+        break;
+      case 1:
+        setVouchers([
+          { id: 7, name: "$1 Drinks Voucher", coins: 70, count: 0 },
+          { id: 8, name: "$1 Food Voucher", coins: 40, count: 0 },
+          { id: 9, name: "$2 Starbucks Voucher", coins: 70, count: 0 },
+          { id: 10, name: "$2 LiHo Voucher", coins: 70, count: 0 },
+          { id: 11, name: "$2 Spinelli Voucher", coins: 70, count: 0 },
+        ]);
+        break;
+      case 2:
+        setVouchers([
+          { id: 12, name: "Forest Green Yarn", coins: 80, count: 0 },
+          { id: 13, name: "Olive Green Yarn", coins: 60, count: 0 },
+          { id: 14, name: "Grass Green Yarn", coins: 80, count: 0 },
+          { id: 15, name: "Yellow Green Yarn", coins: 90, count: 0 },
+          { id: 16, name: "Crochet Hook", coins: 80, count: 0 },
+          { id: 17, name: "Tape", coins: 30, count: 0 },
+        ]);
+        break;
+      case 3:
+        setVouchers([
+          { id: 18, name: "Soil (50g)", coins: 30, count: 0 },
+          { id: 19, name: "Air Plant", coins: 40, count: 0 },
+          { id: 20, name: "Pebbles (50g)", coins: 50, count: 0 },
+          { id: 21, name: "Blue Sand (50g)", coins: 50, count: 0 },
+          { id: 22, name: "Plastic Enclosure", coins: 70, count: 0 },
+          { id: 23, name: "Mini House", coins: 70, count: 0 },
+          { id: 24, name: "Mini Duck", coins: 70, count: 0 },
+          { id: 25, name: "Mini Rabbit", coins: 70, count: 0 },
+        ]);
+        break;
+      default:
+        setVouchers([]);
+        break;
+    }
+  };
 
   const loadPoints = async () => {
     const storedSTPoints = await AsyncStorage.getItem("userPoints");
     setSTPoints(storedSTPoints ? parseInt(storedSTPoints) : 0);
     console.log(`Total points: ${storedSTPoints}`);
   };
-
 
   const deductPoints = async (coins, index) => {
     if (STpoints >= coins) {
@@ -82,8 +80,7 @@ const RewardsItems = ({ route }) => {
       setSTPoints(remainingPoints);
       await AsyncStorage.setItem("userPoints", remainingPoints.toString());
       console.log(`Deducted ${coins} Coins. Remaining: ${remainingPoints}`);
-    } 
-    else {
+    } else {
       console.log("Not enough coins!");
     }
   };
@@ -101,12 +98,6 @@ const RewardsItems = ({ route }) => {
       setVouchers(updatedVouchers);
     }
   };
-
-
-  const totalCost = vouchers.reduce(
-    (acc, item) => acc + item.coins * item.count,
-    0
-  );
 
   const renderVoucher = ({ item, index }) => (
     <View style={styles.voucherCard}>
@@ -136,48 +127,48 @@ const RewardsItems = ({ route }) => {
     </View>
   );
 
-const totalCost = vouchers.reduce((acc, item) => acc + item.coins * item.count, 0);
+  const totalCost = vouchers.reduce((acc, item) => acc + item.coins * item.count, 0);
 
-const handlePurchase = async () => {
-  if (totalCost > STpoints || STpoints === 0) {
-    console.log("Not enough coins for checkout!");
-  } else {
-    const remainingPoints = STpoints - totalCost;
-    setSTPoints(remainingPoints);
-    await AsyncStorage.setItem("userPoints", remainingPoints.toString());
+  const handlePurchase = async () => {
+    if (totalCost > STpoints || STpoints === 0) {
+      console.log("Not enough coins for checkout!");
+    } else {
+      const remainingPoints = STpoints - totalCost;
+      setSTPoints(remainingPoints);
+      await AsyncStorage.setItem("userPoints", remainingPoints.toString());
 
-    const updatedVouchers = vouchers.map((item) => ({ ...item, count: 0 }));
-    setVouchers(updatedVouchers);
+      const updatedVouchers = vouchers.map((item) => ({ ...item, count: 0 }));
+      setVouchers(updatedVouchers);
 
-    console.log("Checkout successful!");
-  }
-};
+      console.log("Checkout successful!");
+    }
+  };
 
-return (
-  <View style={styles.container}>
-    <View style={styles.header}>
-      <View style={styles.pointsContainer}>
-        <View style={styles.centered}>
-          <Image source={require("../../assets/money.png")} style={styles.textImage} />
-          <Text style={styles.pointsText}>{STpoints}</Text>
-          <Text style={styles.totalCoinsText}>Total Coins</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.pointsContainer}>
+          <View style={styles.centered}>
+            <Image source={require("../../assets/money.png")} style={styles.textImage} />
+            <Text style={styles.pointsText}>{STpoints}</Text>
+            <Text style={styles.totalCoinsText}>Total Coins</Text>
+          </View>
         </View>
       </View>
+      <FlatList
+        data={vouchers}
+        renderItem={renderVoucher}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={styles.vouchersContainer}
+      />
+      <View style={styles.checkoutContainer}>
+        <Text style={styles.totalCostText}>Total Cost: {totalCost} Coins</Text>
+        <TouchableOpacity title="Checkout" onPress={handlePurchase} style={styles.checkoutButton}>
+          <Text style={styles.checkoutButtonText}>Checkout</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-    <FlatList
-      data={vouchers}
-      renderItem={renderVoucher}
-      keyExtractor={(item) => item.id.toString()}
-      contentContainerStyle={styles.vouchersContainer}
-    />
-    <View style={styles.checkoutContainer}>
-      <Text style={styles.totalCostText}>Total Cost: {totalCost} Coins</Text>
-      <TouchableOpacity title="Checkout" onPress={handlePurchase} style={styles.checkoutButton}>
-        <Text style={styles.checkoutButtonText}>Checkout</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
-);
+  );
 };
 
 const styles = StyleSheet.create({
@@ -246,7 +237,6 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.2,
     shadowRadius: 6,
-    elevation: 5,
   },
   checkoutContainer: {
     flexDirection: "row",
@@ -293,7 +283,3 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
 });
-
-
-export default RewardsItems;
-
